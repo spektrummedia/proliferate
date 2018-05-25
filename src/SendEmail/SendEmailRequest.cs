0af032e909dad19cb03c6f6a6bb9e008458f50bb
Gabriel Robert <g.robert092@gmail.com>
2018-05-25 16:38:34 -0400

    Basic SendEmail service implementation

---
+++ src/SendEmail/SendEmailRequest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Proliferate
+{
+    public class EmailContent
+    {
+        public string text { get; set; }
+        public string html { get; set; }
+    }
+
+    public class SendEmailRequest
+    {
+        [JsonProperty("api_key")]
+        public string api_key { get; set; }
+
+        [JsonProperty("from")]
+        public string from { get; set; }
+
+        [JsonProperty("to")]
+        public List<string> to { get; set; }
+
+        [JsonProperty("cc")]
+        public List<string> cc { get; set; }
+
+        [JsonProperty("bcc")]
+        public List<string> bcc { get; set; }
+
+        [JsonProperty("subject")]
+        public string subject { get; set; }
+
+        [JsonProperty("content")]
+        public EmailContent content { get; set; }
+    }
+}
---
+++ src/SendEmail/SendEmailService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+using Spk.Common.Helpers.Service;
+using JsonSerializer = Amazon.Lambda.Serialization.Json.JsonSerializer;
+
+[assembly: LambdaSerializer(typeof(JsonSerializer))]
+
+namespace Proliferate
+{
+    public class SendEmailService : FunctionHandler
+    {
+        public Response Execute(Request request, ILambdaContext context)
+        {
+            var result = new ServiceResult<SendEmailRequest>();
+
+            try
+            {
+                var payload = JsonConvert.DeserializeObject<SendEmailRequest>(request.Body);
+                result.SetData(payload);
+            }
+            catch (Exception e)
+            {
+                result.AddError(e.Message);
+            }
+
+            return new Response(result);
+        }
+    }
+}
